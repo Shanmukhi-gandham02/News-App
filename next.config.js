69cefc160a949923ea2a74057269b9f3251968c4
Shanmukhi-gandham02 <84323709+Shanmukhi-gandham02@users.noreply.github.com>
2023-08-20 10:04:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
- 
-    basePath: '/Shanmukhi-gandham02',
+  experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["firebase"],
+    },
    images: {
       remotePatterns: [
         {
